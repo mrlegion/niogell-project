6f221a72e1fee0e985d77f1bbfd386573327d2d3
Alexander Borovskikh <borovskikh.netdevelop@gmail.com>
2019-12-05 11:57:29 +0500

    success connect AdimLte to yii2

--- assets/AdminLteAsset.php
+++ assets/AdminLteAsset.php
@@ -1,15 +1,47 @@
 <?php
 
 
-namespace assets;
+namespace app\assets;
 
 
 use yii\web\AssetBundle;
 
 class AdminLteAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/almasaeed2010/adminlte/dist';
-    public $css = [];
+    public $sourcePath = '@vendor/almasaeed2010/adminlte/';
+    public $css = [
+        'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700',
+        'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
+        'dist/css/adminlte.min.css',
+        'plugins/fontawesome-free/css/all.min.css',
+        'plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
+        'plugins/icheck-bootstrap/icheck-bootstrap.min.css',
+        'plugins/jqvmap/jqvmap.min.css',
+        'plugins/overlayScrollbars/css/OverlayScrollbars.min.css',
+        'plugins/daterangepicker/daterangepicker.css',
+        'plugins/summernote/summernote-bs4.css',
+    ];
 
-    public $js = [];
+    public $js = [
+        'plugins/chart.js/Chart.min.js',
+        'plugins/sparklines/sparkline.js',
+        'plugins/jqvmap/jquery.vmap.min.js',
+        'plugins/jqvmap/maps/jquery.vmap.usa.js',
+        'plugins/jquery-knob/jquery.knob.min.js',
+        'plugins/moment/moment.min.js',
+        'plugins/daterangepicker/daterangepicker.js',
+        'plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
+        'plugins/summernote/summernote-bs4.min.js',
+        'plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js',
+        'dist/js/adminlte.min.js',
+        'dist/js/pages/dashboard.js',
+        'dist/js/demo.js',
+    ];
+
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap4\BootstrapAsset',
+        'yii\bootstrap4\BootstrapPluginAsset',
+        'yii\jui\JuiAsset',
+    ];
 }
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,81 +1,44 @@
 <?php
 
-/* @var $this \yii\web\View */
+use app\assets\AdminLteAsset;
+use app\assets\AppAsset;
+use yii\helpers\Html;
+use yii\web\View;
+
+/* @var $this View */
 /* @var $content string */
 
-use app\widgets\Alert;
-use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
-
+// ! register assets
+AdminLteAsset::register($this);
 AppAsset::register($this);
+
+$directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
+
 ?>
-<?php $this->beginPage() ?>
+
+<?php $this->beginPage(); ?>
+
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php $this->registerCsrfMetaTags() ?>
+    <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+    <?php $this->head(); ?>
 </head>
-<body>
-<?php $this->beginBody() ?>
+<body class="hold-transition sidebar-mini">
+<?php $this->beginBody(); ?>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
-    NavBar::end();
-    ?>
-
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
+<div class="wrapper">
+    <?= $this->render('header', compact('directoryAsset')) ?>
+    <?= $this->render('left', compact('directoryAsset')) ?>
+    <?= $this->render('content', compact('directoryAsset', 'content')) ?>
 </div>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
-
-<?php $this->endBody() ?>
+<?= $this->render('footer') ?>
+<?php $this->endBody(); ?>
 </body>
 </html>
-<?php $this->endPage() ?>
+
+<?php $this->endPage(); ?>
