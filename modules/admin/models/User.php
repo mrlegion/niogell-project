85b38c18508caf863a76c2d015cef37b0166fc62
Alexander Borovskikh <borovskikh.netdevelop@gmail.com>
2019-12-27 18:25:57 +0500

    update logic in User model

--- modules/admin/models/User.php
+++ modules/admin/models/User.php
@@ -2,8 +2,11 @@
 
 namespace app\modules\admin\models;
 
+use app\modules\admin\Module;
 use Yii;
 use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
 use yii\web\IdentityInterface;
 
 /**
@@ -24,6 +27,10 @@ use yii\web\IdentityInterface;
  */
 class User extends \yii\db\ActiveRecord implements IdentityInterface
 {
+    const BLOCKED = 1;
+    const UNBLOCKED = 0;
+    const DELETED = -1;
+
     /**
      * {@inheritdoc}
      */
@@ -37,13 +44,26 @@ class User extends \yii\db\ActiveRecord implements IdentityInterface
         return self::findOne(['username' => $username]);
     }
 
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class'      => TimestampBehavior::class,
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+            ],
+        ];
+    }
+
     /**
      * {@inheritdoc}
      */
     public function rules()
     {
         return [
-            [['email', 'username', 'first_name', 'last_name', 'password', 'access_token', 'auth_key'], 'required'],
+            [['email', 'username', 'first_name', 'last_name', 'password'], 'required'],
             [['is_blocked'], 'integer'],
             [['created_at', 'updated_at'], 'safe'],
             [['email', 'username', 'first_name', 'last_name', 'password', 'access_token', 'auth_key', 'verify_token'], 'string', 'max' => 255],
@@ -56,27 +76,23 @@ class User extends \yii\db\ActiveRecord implements IdentityInterface
     public function attributeLabels()
     {
         return [
-            'id'           => Yii::t('user', 'ID'),
-            'email'        => Yii::t('user', 'Email'),
-            'username'     => Yii::t('user', 'Username'),
-            'first_name'   => Yii::t('user', 'First Name'),
-            'last_name'    => Yii::t('user', 'Last Name'),
-            'password'     => Yii::t('user', 'Password'),
-            'access_token' => Yii::t('user', 'Access Token'),
-            'auth_key'     => Yii::t('user', 'Auth Key'),
-            'verify_token' => Yii::t('user', 'Verify Token'),
-            'is_blocked'   => Yii::t('user', 'Is Blocked'),
-            'created_at'   => Yii::t('user', 'Created At'),
-            'updated_at'   => Yii::t('user', 'Updated At'),
+            'id'           => 'ID',
+            'email'        => 'Email',
+            'username'     => 'Username',
+            'first_name'   => 'First Name',
+            'last_name'    => 'Last Name',
+            'password'     => 'Password',
+            'access_token' => 'Access Token',
+            'auth_key'     => 'Auth Key',
+            'verify_token' => 'Verify Token',
+            'is_blocked'   => 'Is Blocked',
+            'created_at'   => 'Created At',
+            'updated_at'   => 'Updated At',
         ];
     }
 
     /**
-     * Finds an identity by the given ID.
-     * @param string|int $id the ID to be looked for
-     * @return IdentityInterface|null the identity object that matches the given ID.
-     * Null should be returned if such an identity cannot be found
-     * or the identity is not in an active state (disabled, deleted, etc.)
+     * {@inheritdoc}
      */
     public static function findIdentity($id)
     {
@@ -84,13 +100,7 @@ class User extends \yii\db\ActiveRecord implements IdentityInterface
     }
 
     /**
-     * Finds an identity by the given token.
-     * @param mixed $token the token to be looked for
-     * @param mixed $type the type of the token. The value of this parameter depends on the implementation.
-     * For example, [[\yii\filters\auth\HttpBearerAuth]] will set this parameter to be `yii\filters\auth\HttpBearerAuth`.
-     * @return IdentityInterface|null the identity object that matches the given token.
-     * Null should be returned if such an identity cannot be found
-     * or the identity is not in an active state (disabled, deleted, etc.)
+     * {@inheritdoc}
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
@@ -98,8 +108,7 @@ class User extends \yii\db\ActiveRecord implements IdentityInterface
     }
 
     /**
-     * Returns an ID that can uniquely identify a user identity.
-     * @return string|int an ID that uniquely identifies a user identity.
+     * {@inheritdoc}
      */
     public function getId()
     {
@@ -107,21 +116,7 @@ class User extends \yii\db\ActiveRecord implements IdentityInterface
     }
 
     /**
-     * Returns a key that can be used to check the validity of a given identity ID.
-     *
-     * The key should be unique for each individual user, and should be persistent
-     * so that it can be used to check the validity of the user identity.
-     *
-     * The space of such keys should be big enough to defeat potential identity attacks.
-     *
-     * This is required if [[User::enableAutoLogin]] is enabled. The returned key will be stored on the
-     * client side as a cookie and will be used to authenticate user even if PHP session has been expired.
-     *
-     * Make sure to invalidate earlier issued authKeys when you implement force user logout, password change and
-     * other scenarios, that require forceful access revocation for old sessions.
-     *
-     * @return string a key that is used to check the validity of a given identity ID.
-     * @see validateAuthKey()
+     * {@inheritdoc}
      */
     public function getAuthKey()
     {
@@ -129,25 +124,15 @@ class User extends \yii\db\ActiveRecord implements IdentityInterface
     }
 
     /**
-     * Validates the given auth key.
-     *
-     * This is required if [[User::enableAutoLogin]] is enabled.
-     * @param string $authKey the given auth key
-     * @return bool whether the given auth key is valid.
-     * @throws NotSupportedException
-     * @see getAuthKey()
+     * {@inheritdoc}
      */
     public function validateAuthKey($authKey)
     {
-        throw new NotSupportedException();
+        return Yii::$app->security->compareString($this->getAuthKey(), $authKey);
     }
 
     /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     * @throws \yii\base\Exception
+     * {@inheritdoc}
      */
     public function validatePassword($password)
     {
@@ -169,5 +154,19 @@ class User extends \yii\db\ActiveRecord implements IdentityInterface
             return true;
         }
         return false;
+    }
+
+    public function getCreatedAt()
+    {
+        $date = new \DateTime();
+        $date->setTimestamp($this->created_at);
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    public function getUpdatedAt()
+    {
+        $date = new \DateTime();
+        $date->setTimestamp($this->updated_at);
+        return $date->format('Y-m-d H:i:s');
     }
 }
