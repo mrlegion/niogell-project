ab7305de8706ca83bc60faa56629d9c73feb8a64
Alexander Borovskikh <borovskikh.netdevelop@gmail.com>
2019-12-27 18:10:27 +0500

    Add resend method

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,10 +2,13 @@
 
 namespace app\controllers;
 
+use app\models\MailService;
 use app\models\RegisterService;
+use app\models\Vote;
 use app\models\VoteForm;
 use RuntimeException;
 use Yii;
+use yii\base\ExitException;
 use yii\web\Controller;
 
 class SiteController extends Controller
@@ -24,11 +27,11 @@ class SiteController extends Controller
     public function actions()
     {
         return [
-            'error'   => [
+            'error' => [
                 'class' => 'yii\web\ErrorAction',
             ],
             'captcha' => [
-                'class'           => 'yii\captcha\CaptchaAction',
+                'class' => 'yii\captcha\CaptchaAction',
                 'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
             ],
         ];
@@ -59,6 +62,7 @@ class SiteController extends Controller
                 } catch (\Exception $e) {
                     Yii::$app->errorHandler->logException($e);
                     Yii::$app->session->setFlash('RegisterError', $e->getMessage());
+                    throw $e;
                 }
             }
         }
@@ -67,9 +71,33 @@ class SiteController extends Controller
     }
 
     // ! There is no action on pressing the button to resend the message
-    public function actionSuccess() {
+    public function actionSuccess()
+    {
         // TODO: Make a method to resend a message to a user
         return $this->render('success', ['email' => Yii::$app->request->get('email')]);
+    }
+
+    public function actionResend(string  $email)
+    {
+        $vote = Vote::findByEmail($email);
+        if ($vote && $vote->status === Vote::INACTIVE) {
+            try {
+                // ? Maybe need create method or operation for generate new verify token when we resend email
+                // TODO: create method or operation for generate new verify token when we resend email
+                // ! Do it wrong. Add Transactions
+                $vote->setAttribute('verify_token', Yii::$app->security->generateRandomString());
+                MailService::sendEmail($vote);
+                $vote->save();
+                return $this->redirect(['site/success', 'email' => $vote->email]);
+            } catch (\Swift_TransportException $e) {
+                Yii::$app->errorHandler->logException($e);
+                Yii::$app->session->setFlash('ResendEmailError', $e->getMessage());
+            } catch (\Exception $e) {
+                Yii::$app->errorHandler->logException($e);
+                Yii::$app->session->setFlash('ResendEmailError', $e->getMessage());
+            }
+        }
+        return $this->render('error', ['name' => 'Error on resend', 'message' => 'On resend email has error! Please try again later']);
     }
 
     // ! There is no action on the wrong token from the user
@@ -79,10 +107,19 @@ class SiteController extends Controller
         $service = new RegisterService();
         try {
             $service->confirmation($token);
-        } catch (RuntimeException $e) {
+        } catch (ExitException $e) {
             Yii::$app->errorHandler->logException($e);
             Yii::$app->session->setFlash('error', $e->getMessage());
+            return $this->render('error', ['name' => $e->getCode(), 'message' => $e->getMessage()]);
         }
         return $this->render('email-confirm');
+    }
+
+    public function actionError()
+    {
+        $exception = Yii::$app->errorHandler->exception;
+        if ($exception !== null) {
+            return $this->render('error', ['exception' => $exception, 'name' => $exception->getCode(), 'message' => $exception->getMessage()]);
+        }
     }
 }
