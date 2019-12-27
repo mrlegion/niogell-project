be0ab8597a309d45d9c45b238da82593c1e75d14
Alexander Borovskikh <borovskikh.netdevelop@gmail.com>
2019-12-27 18:22:40 +0500

    added custom translation in module (does not work T_T)

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -2,6 +2,11 @@
 
 namespace app\modules\admin;
 
+use Yii;
+use yii\filters\AccessControl;
+use yii\i18n\PhpMessageSource;
+use yii\web\ErrorHandler;
+
 /**
  * admin module definition class
  */
@@ -15,11 +20,47 @@ class Module extends \yii\base\Module
 
     /**
      * {@inheritdoc}
+     * @throws \yii\base\InvalidConfigException
      */
     public function init()
     {
         parent::init();
 
         // custom initialization code goes here
+        $this->registerTranslations();
+
+        Yii::configure($this, [
+            'components' => [
+                'errorHandler' => [
+                    'class' => ErrorHandler::class,
+                    'errorAction' => '/admin/auth/error'
+                ],
+            ],
+        ]);
+
+        /* @var ErrorHandler $handler */
+        $handler = $this->get('errorHandler');
+        Yii::$app->set('errorHandler', $handler);
+        $handler->register();
+
     }
+
+    public function registerTranslations()
+    {
+        Yii::$app->i18n->translations['admin/*'] = [
+            'class'          => PhpMessageSource::class,
+            'basePath'       => '@app/messages/',
+            'sourceLanguage' => 'en-US',
+            'fileMap' => [
+                'admin/user' => 'user.php',
+            ],
+        ];
+    }
+
+    public static function t($category, $message, $params = [], $language = null)
+    {
+        return Yii::t('admin/' . $category, $message, $params, $language);
+
+    }
+
 }
