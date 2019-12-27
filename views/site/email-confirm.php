cbe87dce6ae34cd49554dca768c1d22ff8ef9d86
Alexander Borovskikh <borovskikh.netdevelop@gmail.com>
2019-12-27 18:17:52 +0500

    changing confirm text and title

--- views/site/email-confirm.php
+++ views/site/email-confirm.php
@@ -2,12 +2,11 @@
 
 use yii\web\View;
 
-
-
 /* @var $this View */
 
-$this->title = 'Email confirmation';
+$this->title = 'Your email has confirm';
 
 ?>
 
-
+<h2>Your email has confirmation</h2>
+<h4>Now you can see other information on site</h4>
