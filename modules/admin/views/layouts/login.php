c7e8d3b9475b476bf8ed67cf240c570344dc08e9
Alexander Borovskikh <borovskikh.netdevelop@gmail.com>
2019-12-27 18:25:10 +0500

    added layout for login form

---
+++ modules/admin/views/layouts/login.php
@@ -0,0 +1,32 @@
+<?php
+
+use app\assets\AdminLteAsset;
+use yii\helpers\Html;
+use yii\web\View;
+
+/* @var $this View */
+/* @var $content string */
+
+AdminLteAsset::register($this);
+
+?>
+<?php $this->beginPage() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body class="hold-transition login-page">
+
+    <?php $this->beginBody() ?>
+
+    <?= $content ?>
+
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+<?php $this->endPage() ?>
