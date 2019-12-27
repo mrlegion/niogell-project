719f3088ca946ce3e7b7d40c897637d0b27e56b2
Alexander Borovskikh <borovskikh.netdevelop@gmail.com>
2019-12-27 18:23:23 +0500

    base logic for User controller in admin module

--- modules/admin/controllers/UserController.php
+++ modules/admin/controllers/UserController.php
@@ -2,11 +2,48 @@
 
 namespace app\modules\admin\controllers;
 
+use yii\filters\AccessControl;
+
 class UserController extends \yii\web\Controller
 {
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['*'],
+                        'roles' => ['admin']
+                    ],
+                ],
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
     }
 
+    public function actionCreate()
+    {
+        return $this->render('create');
+    }
+
+    public function actionView(int $id)
+    {
+        return $this->render('view');
+    }
+
+    public function actionUpdate(int $id)
+    {
+        return $this->render('update');
+    }
+
+    public function actionDelete(int $id)
+    {
+        return $this->render('delete');
+    }
 }
