493e2e7ac9464602779d09cda2188a58eecd1691
Alexander Borovskikh <borovskikh.netdevelop@gmail.com>
2019-12-28 23:09:01 +0500

    added link to added new category in options

--- modules/admin/views/option/index.php
+++ modules/admin/views/option/index.php
@@ -1,11 +1,20 @@
 <?php
 
+use yii\helpers\Html;
 use yii\web\View;
 
 
 /* @var $this View */
 
-
+$this->title = 'Options control panel';
 ?>
 
-<h1>Options Index</h1>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <?= Html::a('<i class="fas fa-plus"></i> Create', ['option/create-category'], ['class' => 'btn btn-primary']) ?>
+            </div>
+        </div>
+    </div>
+</div>
