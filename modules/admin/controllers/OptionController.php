1bdbd03927386c72783b4efe27b5b4c4a7189a65
Alexander Borovskikh <borovskikh.netdevelop@gmail.com>
2019-12-28 23:10:32 +0500

    added logic in options controller from added category method

--- modules/admin/controllers/OptionController.php
+++ modules/admin/controllers/OptionController.php
@@ -4,6 +4,10 @@
 namespace app\modules\admin\controllers;
 
 
+use app\modules\admin\models\CategoryForm;
+use app\modules\admin\models\OptionForm;
+use app\modules\admin\models\OptionTitle;
+use yii\di\Container;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 
@@ -31,18 +35,36 @@ class OptionController extends Controller
 
     public function actionIndex()
     {
+        // todo: need create form for editing options value
 
         return $this->render('index');
     }
 
     public function actionSave()
     {
-
+        if (\Yii::$app->request->isPost) {
+            // todo: create logic for saved changed options
+        }
+        return $this->redirect(['option/index']);
     }
 
     public function actionCreateCategory()
     {
-        return $this->render('create-category');
+        $optionForm = new CategoryForm();
+
+        // get post params
+        if (\Yii::$app->request->isPost) {
+            // todo: create logic for adding new record in db
+            if ($optionForm->load(\Yii::$app->request->post()) && $optionForm->validate()) {
+                if ($optionForm->save()) {
+                    return $this->redirect(['option/index']);
+                }
+            }
+        }
+
+        $categories = OptionTitle::getCategories();
+
+        return $this->render('create-category', compact('optionForm', 'categories'));
     }
 
     public function actionCreateOption()
