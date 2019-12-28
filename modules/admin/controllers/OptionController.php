95ab9bf13d808dc77c2ac1232fecfef1b28ec163
Alexander Borovskikh <borovskikh.netdevelop@gmail.com>
2019-12-28 13:49:27 +0500

    Created options controller for admin panel

---
+++ modules/admin/controllers/OptionController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace app\modules\admin\controllers;
+
+
+use yii\web\Controller;
+
+class OptionController extends Controller
+{
+    public function actionIndex()
+    {
+
+    }
+
+    public function actionSave()
+    {
+
+    }
+}
