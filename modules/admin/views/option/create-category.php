ee2770772ffd132da31785a0de1a493707947cca
Alexander Borovskikh <borovskikh.netdevelop@gmail.com>
2019-12-28 23:09:48 +0500

    added form for added new category in options

--- modules/admin/views/option/create-category.php
+++ modules/admin/views/option/create-category.php
@@ -1,10 +1,37 @@
 <?php
 
+use app\modules\admin\models\CategoryForm;
+use yii\helpers\Html;
 use yii\web\View;
+use yii\widgets\ActiveForm;
 
 
 /* @var $this View */
+/* @var $categories array */
+/* @var $optionForm CategoryForm */
+
+$this->title = Yii::t('option', 'Added new Category into Options');
+$this->params['Breadcrumbs'] = $this->title;
 
 ?>
 
-<h1>Options Create Category</h1>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <p>Select category or create uniq</p>
+                <p>After entered name for this category and click save button</p>
+            </div>
+            <div class="card-body">
+                <?php $form = ActiveForm::begin(); ?>
+                <?= $form->field($optionForm, 'name')->textInput() ?>
+                <?= $form->field($optionForm, 'category')->dropDownList($categories, ['disabled' => empty($categories) ]) ?>
+                <?= $form->field($optionForm, 'isRoot')->checkbox(['disabled' => empty($categories)]) ?>
+                <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+                <?= Html::a('Cancel', ['option/index'], ['class' => 'btn btn-danger float-right']) ?>
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
